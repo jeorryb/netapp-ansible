a2d42eaa37f138a3a41eaa954b8bc1cc99b8909c
jeorryb <jeorry@gmail.com>
2017-04-10 13:09:21 -0400

    change ssl verification

--- library/date_time.py
+++ library/date_time.py
@@ -62,56 +62,81 @@ EXAMPLES = '''
 
 def date_time(module):
 
-  timezone = module.params['timezone']
-  date = module.params['date']
+    cluster = module.params['cluster']
+    user_name = module.params['user_name']
+    password = module.params['password']
+    val_certs = module.params['val_certs']
+    timezone = module.params['timezone']
+    date = module.params['date']
 
-  results = {}
+    connection = NaServer(cluster, 1 , 0)
+    connection.set_server_type("FILER")
+    connection.set_transport_type("HTTPS")
+    connection.set_port(443)
+    connection.set_style("LOGIN")
+    connection.set_admin_user(user_name, password)
 
-  results['changed'] = False
+    if not val_certs:
+        try:
+            _create_unverified_https_context = ssl._create_unverified_context
+        
+        except AttributeError:
+        # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+        else:
+        # Handle target environment that doesn't support HTTPS verification
+            ssl._create_default_https_context = _create_unverified_https_context
 
+    results = {}
 
-  args = NaElement("args")
-
-  args.child_add(NaElement("arg", "cluster"))
-  args.child_add(NaElement("arg", "date"))
-  args.child_add(NaElement("arg", "modify"))
-  if module.params['timezone']:
-    args.child_add(NaElement("arg", "-timezone"))
-    args.child_add(NaElement("arg", timezone))
-  if module.params['date']:
-    args.child_add(NaElement("arg", "-date"))
-    args.child_add(NaElement("arg", date))
-
-  systemCli = NaElement("system-cli")
-  systemCli.child_add(args)
-  xo = connect_to_api.invoke_elem(systemCli)
-
-  if(xo.results_errno() != 0):
-    r = xo.results_reason()
-    module.fail_json(msg=r)
     results['changed'] = False
 
-  else:
-    results['changed'] = True
 
-  return results
+    args = NaElement("args")
+
+    args.child_add(NaElement("arg", "cluster"))
+    args.child_add(NaElement("arg", "date"))
+    args.child_add(NaElement("arg", "modify"))
+    if module.params['timezone']:
+        args.child_add(NaElement("arg", "-timezone"))
+        args.child_add(NaElement("arg", timezone))
+    if module.params['date']:
+        args.child_add(NaElement("arg", "-date"))
+        args.child_add(NaElement("arg", date))
+
+    systemCli = NaElement("system-cli")
+    systemCli.child_add(args)
+    xo = connection.invoke_elem(systemCli)
+
+    if(xo.results_errno() != 0):
+        r = xo.results_reason()
+        module.fail_json(msg=r)
+        results['changed'] = False
+
+    else:
+        results['changed'] = True
+
+    return results
 
 def main():
-  argument_spec = ntap_argument_spec()
-  argument_spec.update(dict(
+  module = AnsibleModule(
+    argument_spec = dict(
+      cluster=dict(required=True),
+      user_name=dict(required=True),
+      password=dict(required=True),
+      val_certs=dict(type='bool', default=True),
       timezone=dict(required=False),
-      date=dict(required=True)))
-  
-  module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
-
+      date=dict(required=True),
+    ), 
+      supports_check_mode=False
+  )
   results = date_time(module)
 
   
 
   module.exit_json(**results)
 
-from ansible.module_utils.basic import *
-from ntap_util import * 
+from ansible.module_utils.basic import * 
 main()
 
 
