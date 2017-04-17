9f9fb8e591d6d7bf668e1a82b58ab46fda72044b
jeorryb <jeorry@gmail.com>
2017-04-17 14:34:58 -0400

    Add no_log to password parameter

---
+++ module_utils/ntap_util.py
@@ -0,0 +1,44 @@
+
+from ansible.module_utils.basic import *
+import ssl
+from NaServer import *
+
+def ntap_argument_spec():
+    return dict(
+        cluster=dict(required=True),
+        user_name=dict(required=True),
+        password=dict(required=True, no_log=True),
+        validate_certs=dict(type='bool', default=True),
+    )
+
+def invoke_ssl_no_verify():
+    try:
+        _create_unverified_https_context = ssl._create_unverified_context
+        
+    except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+        pass
+    else:
+    # Handle target environment that doesn't support HTTPS verification
+        ssl._create_default_https_context = _create_unverified_https_context
+
+
+def connect_to_api(module, vserver=None):
+    cluster = module.params['cluster']
+    user_name = module.params['user_name']
+    password = module.params['password']
+    validate_certs = module.params['validate_certs']
+
+    if not validate_certs:
+        invoke_ssl_no_verify()
+
+    connection = NaServer(cluster, 1 , 0)
+    connection.set_server_type("FILER")
+    connection.set_transport_type("HTTPS")
+    connection.set_port(443)
+    connection.set_style("LOGIN")
+    connection.set_admin_user(user_name, password)
+    if vserver:
+        connection.set_vserver(vserver)
+    return connection
+
